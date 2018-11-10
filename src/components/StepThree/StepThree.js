fdb88b3741632ac369b5805d5d6052e8de1e167e
Jackson Taylor <josephjacksontaylor@gmail.com>
2018-11-10 16:10:11 -0700

    fixed issue where mortgage and rent were always 0

--- src/components/StepThree/StepThree.js
+++ src/components/StepThree/StepThree.js
@@ -8,15 +8,15 @@ class StepThree extends Component {
     constructor() {
         super();
         this.state = {
-            mortgage: 0,
-            rent: 0,
+            // mortgage: 0,
+            // rent: 0,
         }
 
         this.postHouse = this.postHouse.bind(this);
     }
 
-    // handleMortgageChange(value) {this.setState({mortgage: +value})}
-    // handleRentChange(value) {this.setState({rent: +value})}
+    handleMortgageChange = (value) => {this.setState({mortgage: +value})}
+    handleRentChange = (value) => {this.setState({rent: +value})}
 
     componentDidMount() {
         this.setState({
@@ -40,10 +40,10 @@ class StepThree extends Component {
         // console.log(this.props)
         return (
             <div>
-                <input value={this.props.mortgage} placeholder='Mortgage' onChange={e=> this.props.updateMortgage(e.target.value)}/>
-                <input value={this.props.rent} placeholder='Rent' onChange={e=> this.props.updateRent(e.target.value)}/>
+                <input value={this.state.mortgage} placeholder='Mortgage' onChange={e=> this.handleMortgageChange(e.target.value)}/>
+                <input value={this.state.rent} placeholder='Rent' onChange={e=> this.handleRentChange(e.target.value)}/>
                 <Link to='/wizard/step2'><button>Previous Step</button></Link>
-                 <button onClick={() => this.postHouse()}>Complete</button>
+                 <button onClick={() => {this.postHouse();}}>Complete</button>
             </div>
         )
     }
